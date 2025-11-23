a177c6dc1afd85f7c9d6e3b7c903921abf5b15b2
pjdpaulsagnik <sagnikpaul81@gmail.com>
2025-11-23 20:35:22 +0530

    updating latest version - 0.0.4 with fixes

--- www/backgroundAppRun.js
+++ www/backgroundAppRun.js
@@ -1,34 +1,27 @@
-cordova.define("backgroundapprun.backgroundAppRun", function(require, exports, module) {
-	var exec = require('cordova/exec');
-	
-	backRun ={
-		startServ : function( successCallback, errorCallback ){
-			cordova.exec(successCallback, errorCallback, 'backgroundAppRun', 'startBackgroundService')
-		},
-		stopServ : function(decryptedFilePath, successCallback, errorCallback ){
-			cordova.exec(decryptedFilePath, successCallback, errorCallback, 'backgroundAppRun', 'stopBackgroundService')
-		},
-		  startSoundServ : function( successCallback, errorCallback ){
-			cordova.exec(successCallback, errorCallback, 'backgroundAppRun', 'startMakeSoundBackgroundService')
-		},
-		stopSoundServ : function( successCallback, errorCallback ){
-			cordova.exec(successCallback, errorCallback, 'backgroundAppRun', 'stopMakeSoundBackgroundService')
-		},
-		  coolMethod : function( successCallback, errorCallback ){
-			cordova.exec(successCallback, errorCallback, 'backgroundAppRun', 'coolMethod')
-		},
-		fireEvent: function(eventData, successCallback, errorCallback) {
-			cordova.exec(successCallback, errorCallback, 'backgroundAppRun', 'fireEvent', [eventData]);
-		},
-		backgroundDeleteFile: function(decryptedFilePath, successCallback, errorCallback) {
-			exec(successCallback, errorCallback, 'backgroundAppRun', 'backgroundDeleteService', [decryptedFilePath]);
-		}
+var exec = require('cordova/exec');
+
+var backRun = {
+	startServ : function( successCallback, errorCallback ){
+		cordova.exec(successCallback, errorCallback, 'backgroundAppRun', 'startBackgroundService')
+	},
+	stopServ : function( successCallback, errorCallback ){
+		cordova.exec(successCallback, errorCallback, 'backgroundAppRun', 'stopBackgroundService')
+	},
+	startSoundServ : function( successCallback, errorCallback ){
+		cordova.exec(successCallback, errorCallback, 'backgroundAppRun', 'startMakeSoundBackgroundService')
+	},
+	stopSoundServ : function( successCallback, errorCallback ){
+		cordova.exec(successCallback, errorCallback, 'backgroundAppRun', 'stopMakeSoundBackgroundService')
+	},
+	coolMethod : function( successCallback, errorCallback ){
+		cordova.exec(successCallback, errorCallback, 'backgroundAppRun', 'coolMethod')
+	},
+	fireEvent: function(eventData, successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, 'backgroundAppRun', 'fireEvent', [eventData]);
+	},
+	backgroundDeleteFile: function(decryptedFilePath, successCallback, errorCallback) {
+		exec(successCallback, errorCallback, 'backgroundAppRun', 'backgroundDeleteService', [decryptedFilePath]);
 	}
-	module.exports = backRun;
-	
-	// exports.coolMethod = function (arg0, success, error) {
-	//     exec(success, error, 'backgroundAppRun', 'coolMethod', [arg0]);
-	// };
-	
-	});
-	
+};
+
+module.exports = backRun;
